8bdd6cbb155f5f4dc43cd4bc3ef364bda32ad49a
kajela <titoyut56@gmail.com>
2022-05-10 07:00:19 -0400

    Initial commit

---
+++ src/food.java
@@ -0,0 +1,19 @@
+import com.sun.management.VMOption;
+
+import javax.swing.*;
+import java.io.Console;
+
+public class food {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        int number = 10;
+        System.out.println("enter your number");
+        if (number == 10) {
+            System.out.println("its even number");
+        }else {
+                System.out.println("its odd number");
+            }
+        }
+
+    }
+
